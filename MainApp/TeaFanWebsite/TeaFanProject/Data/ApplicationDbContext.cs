2c201c1abd6a89a3cb3fe5d7d32821a7773ff564
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-09-10 21:47:28 +0700

    Run react app success

---
+++ MainApp/TeaFanWebsite/TeaFanProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeaFanProject.Data
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
