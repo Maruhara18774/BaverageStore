f3b4f4ca96f5ca7a0bcc086526e87af6403490cc
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-09-15 15:05:23 +0700

    Login onsubmit

--- MainApp/TeaFanWebsite/TeaFanProject/FrontEnd/src/Pages/Login/index.js
+++ MainApp/TeaFanWebsite/TeaFanProject/FrontEnd/src/Pages/Login/index.js
@@ -1,6 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LoginApi from '../../Api/LoginApi';
 
 export class LoginPage extends Component {
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        var email = e.target.username.value;
+        var password = e.target.password.value;
+        LoginApi.login({
+          email: email,
+          password: password,
+        })
+          .then((data) => {
+            if (data.statusCode !== 200) {  /* Wrong username or password */}
+            else {
+                localStorage.setItem("user", JSON.stringify(data.data));
+                history.push("/home");
+            }
+          })
+          .catch((err) => console.error(err));
+      };
     render() {
         return (
             <div>
