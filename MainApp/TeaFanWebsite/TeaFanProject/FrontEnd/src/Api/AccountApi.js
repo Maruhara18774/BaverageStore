810fe77312a02d603ea55e09a5aec2e75297bd6b
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-09-21 21:02:33 +0700

    Profie gateway

--- MainApp/TeaFanWebsite/TeaFanProject/FrontEnd/src/Api/AccountApi.js
+++ MainApp/TeaFanWebsite/TeaFanProject/FrontEnd/src/Api/AccountApi.js
@@ -12,5 +12,15 @@ const AccountApi = {
       console.error(error);
     }
   },
+  logout: async () =>{
+    try {
+      const url = baseURL + "/Logout";
+      var result = await API.get(url);
+      return result;
+    } catch (error) {
+      //alert(error);
+      console.error(error);
+    }
+  }
 };
 export default AccountApi;
--- MainApp/TeaFanWebsite/TeaFanProject/FrontEnd/src/Pages/Profie/index.js
+++ MainApp/TeaFanWebsite/TeaFanProject/FrontEnd/src/Pages/Profie/index.js
@@ -19,13 +19,11 @@ export class Profie extends Component {
     changePosition(position){
         this.setState({position: position})
     }
-    render() {
+    getContent(){
         switch(this.state.position){
             case "detail":
                 return (
-                    <div className = "wrap">
-                        <SubNavbar name={this.state.user.lastName} changePosition={(val)=>this.changePosition(val)}/>
-                        <div className="profie-content">
+                    <div className="profie-content">
                             <table style={{width:"100%", marginBottom:"10px"}}>
                                 <tr>
                                     <td>First Name:</td>
@@ -48,15 +46,39 @@ export class Profie extends Component {
                                     <td>{this.state.user.address}</td>
                                 </tr>
                             </table>
-                            <Button shape="round" style={{marginRight:"20px"}}>Change Password</Button>
-                            <Button shape="round">Edit Details</Button>
+                            <Button 
+                            shape="round" 
+                            style={{marginRight:"20px"}} 
+                            onClick={this.setState({position:"changePassword"})}>
+                                Change Password</Button>
+                            <Button shape="round" 
+                            onClick={this.setState({position:"edit"})}>
+                                Edit Details</Button>
                         </div>
-                    </div>
                 )
+            case "history":
+                return(<div className="profie-content">
+                History
+                </div>)
+            case "changePassword":
+                return(<div className="profie-content">
+                    Change Password
+                </div>)
+            case "edit":
+                return(<div className="profie-content">
+                Edit profie
+                </div>)
             default:
                 break;
         }
-        
+    }
+    render() {
+        return(
+            <div className="wrap">
+                <SubNavbar name={this.state.user.lastName} changePosition={(val)=>this.changePosition(val)}/>
+                {this.getContent()}
+            </div>
+        )
     }
 }
 
